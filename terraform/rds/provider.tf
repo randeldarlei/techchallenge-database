4a055f6849dfd8025dd43af886bbe53599d96eee
Darlei Randel Oliveira Menezes <darley_rom@hotmail.com>
2024-12-10 12:06:56 -0300

    Fix Terraform action

--- terraform/rds/provider.tf
+++ terraform/rds/provider.tf
@@ -1,15 +1,29 @@
-data "terraform_remote_state" "network" {
-  backend = "remote"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 
-  config = {
+  backend "remote" {
     hostname     = "app.terraform.io"
     organization = "ACG-Terraform-Labs-Teste"
-    workspaces = {
+
+    workspaces {
       name = "lab-migrate-state"
     }
   }
 }
 
-output "vpc_id" {
-  value = data.terraform_remote_state.network.outputs.vpc_id
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "us-east-1"
 }
