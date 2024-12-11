a4bafc0b60f3433f5bc78e89224c4c83da8d2e94
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-11 20:42:21 -0300

    Update provider.tf

--- terraform/rds/provider.tf
+++ terraform/rds/provider.tf
@@ -15,15 +15,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {
-  default = "us-east-1"
-}
