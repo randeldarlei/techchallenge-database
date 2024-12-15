36f15292c4061dd812dc0a0f89f306888ad0c26e
SamuelOFernandes <samuel.o.fernandes2020@gmail.com>
2024-12-15 20:29:21 -0300

    fix aws provider

--- terraform/rds/provider.tf
+++ terraform/rds/provider.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "ACG-Terraform-Labs-Teste"
-
-    workspaces {
-      name = "techchallenge-database"
-    }
-  }
-}
-
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
-}
-
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {
-  default = "us-east-1"
+  region = "us-east-2" 
 }
