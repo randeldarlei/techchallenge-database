b47c202bf70abaa4cc1fbccdf076f7295d4a6a80
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-17 19:39:10 -0300

    Update rds.tf

--- terraform/rds/rds.tf
+++ terraform/rds/rds.tf
@@ -14,20 +14,14 @@ resource "aws_db_instance" "techchallenge-rds" {
   apply_immediately        = true
   multi_az                 = false
 
-  # Referência ao Security Group do EKS que permite acesso ao RDS
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-
+  vpc_security_group_ids      = [data.terraform_remote_state.network.outputs.aws_security_group_id]
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+
 }
 
-# Subnet Group para o RDS nas subnets privadas
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = [
-    aws_subnet.private_cluster_subnet_1.id,
-    aws_subnet.private_cluster_subnet_2.id,
-    aws_subnet.private_cluster_subnet_3.id
-  ]
+  subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
 
   tags = {
     Name = "rds-subnet-group"
