6ea5e65993bcf760e02b4d9b407eb94d7d6fb93d
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-18 17:11:18 -0300

    Update rds.tf

--- terraform/rds/rds.tf
+++ terraform/rds/rds.tf
@@ -1,31 +1,28 @@
-resource "aws_db_instance" "techchallenge-rds" {
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = data.terraform_remote_state.network.outputs.public_subnet_ids
+
+  tags = {
+    Name = "RDS Subnet Group"
+  }
+}
+
+resource "aws_db_instance" "techchallenge_rds" {
   allocated_storage        = 20
   engine                   = "postgres"
   engine_version           = "16.1"
-  identifier               = "techchallenge-rds"
   instance_class           = "db.t3.micro"
-  storage_encrypted        = false
   publicly_accessible      = true
   delete_automated_backups = true
   skip_final_snapshot      = true
   db_name                  = "techchallenge"
   username                 = "master"
   password                 = "0dG3y771"
+  vpc_security_group_ids   = [data.terraform_remote_state.network.outputs.aws_security_group_id]
+  db_subnet_group_name     = aws_db_subnet_group.rds_subnet_group.name
   apply_immediately        = true
-  multi_az                 = false
-
-  vpc_security_group_ids = [data.terraform_remote_state.network.outputs.aws_security_group_id]
-  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-}
-
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "rds-subnet-group"
-  subnet_ids = [
-    aws_subnet.public_cluster_subnet_1.id,
-    aws_subnet.public_cluster_subnet_2.id
-  ]
 
   tags = {
-    Name = "RDS Subnet Group"
+    Name = "TechChallenge RDS"
   }
 }
