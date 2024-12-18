f6b48bea034b29e05b94a3f21d472f17fe8dd301
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-18 16:14:42 -0300

    Update rds.tf

--- terraform/rds/rds.tf
+++ terraform/rds/rds.tf
@@ -14,14 +14,13 @@ resource "aws_db_instance" "techchallenge-rds" {
   apply_immediately        = true
   multi_az                 = false
 
-  vpc_security_group_ids      = [data.terraform_remote_state.network.outputs.aws_security_group_id]
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = data.terraform_remote_state.network.outputs.private_subnet_ids
+  subnet_ids = data.terraform_remote_state.network.outputs.public_subnet_ids
 
   tags = {
     Name = "rds-subnet-group"
